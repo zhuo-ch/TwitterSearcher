48039db7f6e7c2fad0108ab9013119a3447b2a4e
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-03-25 17:28:51 -0400

    Add requests for twitter api.

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 const express = require('express');
 const request = require('request');
+const rp = require('request-promise');
 const path = require('path');
-const twitter = require('./config.js');
+const twitter = require('./lib/config').twitter;
+const twitterAPI = require('./lib/twitter_api');
 const app = express();
 
 app.use(express.static(`${__dirname}/lib`));
+app.use(express.static(`${__dirname}/node_modules`));
 
 app.get('/search/:query', (req, res) => {
   const hashTag = req.params.query;
-  console.log(hashTag);
-  fetch(twitter.search + 'q=' + hashTag)
-    .then(response => response.json())
-    .then(resolve => resolve.pipe(res))
-  // res.json({ result: hashTag });
+  const body = twitterAPI.genTwitter(twitter.searchUrl + 'q=' + hashTag);
+  const bearer = twitterAPI.genTwitterBearer();
+  console.log(bearer);
+
+  request.post(bearer, function(e, r, b) {
+    console.log('e:' + e, 'r:' + r, 'b:' + b)
+  })
+    // .then(response => {
+    //   console.log(response)
+    //   // response.json()
+    // })
+    // .catch(err => console.log(err))
+
+    // .then(resolve => {
+    //   console.log(resolve);
+    //   resolve.pipe(res)
+    // });
+  // res.json({ result: request });
 });
 
 app.listen(8000, console.log('hello world'));
-
-// $ curl --request GET
-//  --url 'https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular'
-//  --header 'authorization: OAuth oauth_consumer_key="consumer-key-for-app",
-//  oauth_nonce="generated-nonce", oauth_signature="generated-signature",
-//  oauth_signature_method="HMAC-SHA1", oauth_timestamp="generated-timestamp",
-//  oauth_token="access-token-for-authed-user", oauth_version="1.0"'
-
-// OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog",
-// oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg",
-// oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D",
-// oauth_signature_method="HMAC-SHA1",
-// oauth_timestamp="1318622958",
-// oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
-// oauth_version="1.0"
-
-nonce = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
